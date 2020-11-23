73f59bd210a94f035354486a4c44bdc1d2548f42
dokines <slashmc4@gmail.com>
2020-11-23 09:28:42 +0100

    Init progetto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mapApp';
+
+  public onMapReady(event) {
+    console.log("Map Ready")
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OpenLayerMapComponent } from './open-layer-map/open-layer-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OpenLayerMapComponent
   ],
   imports: [
     BrowserModule,
